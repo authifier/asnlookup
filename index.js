40991f30c174cf24f715269ea4b56467e95fbc8a
Paul Makles <me@insrt.uk>
2024-06-24 08:03:26 +0100

    refactor: provide AS number instead of name feat: background update routine feat: AS num to name routine

--- index.js
+++ index.js
@@ -1,112 +1,147 @@
-'use strict';
+"use strict";
 
-const fs = require('fs'),
-    path = require('path'),
-    debug = require('debug')('asnlookup'),
-    ipcheck = require('ipcheck'),
-    superagent = require('superagent');
+const fs = require("fs"),
+  path = require("path"),
+  debug = require("debug")("asnlookup"),
+  ipcheck = require("ipcheck"),
+  superagent = require("superagent");
 
-const APNIC_RAW_TABLE_URL = 'http://thyme.apnic.net/current/data-raw-table';
-const APNIC_USED_AUTNUMS_URL = 'http://thyme.apnic.net/current/data-used-autnums';
+const APNIC_RAW_TABLE_URL = "http://thyme.apnic.net/current/data-raw-table";
+const APNIC_USED_AUTNUMS_URL =
+  "http://thyme.apnic.net/current/data-used-autnums";
 const MS_IN_DAYS = 1000 * 60 * 60 * 24;
 
-let gCacheFilePath = '';
+let gCacheFilePath = "";
 let gCache = {
-    timestamp: 0,
-    blocks: {},
-    provider: {}
+  timestamp: 0,
+  blocks: {},
+  provider: {},
 };
 
 function lastUpdated() {
-    if (gCache.timestamp === 0) return Infinity;
+  if (gCache.timestamp === 0) return Infinity;
 
-    const daysLastUpdated = parseInt(gCache.timestamp / MS_IN_DAYS);
-    const daysNow = parseInt(Date.now() / MS_IN_DAYS);
+  const daysLastUpdated = parseInt(gCache.timestamp / MS_IN_DAYS);
+  const daysNow = parseInt(Date.now() / MS_IN_DAYS);
 
-    // something is wrong
-    if (daysLastUpdated > daysNow) return Infinity;
+  // something is wrong
+  if (daysLastUpdated > daysNow) return Infinity;
 
-    return daysNow - daysLastUpdated;
+  return daysNow - daysLastUpdated;
 }
 
 async function update() {
-    debug('update local cache from https://thyme.apnic.net ...');
+  debug("update local cache from https://thyme.apnic.net ...");
 
-    const blocks = {};
-    const provider = {};
+  const blocks = {};
+  const provider = {};
 
-    let blockData = await superagent.get(APNIC_RAW_TABLE_URL);
-    blockData.text.split('\n').filter(function (l) { return !!l; }).forEach(function (l) {
-        const data = l.split('\t');
-        if (data.length !== 2) return;
+  let blockData = await superagent.get(APNIC_RAW_TABLE_URL);
+  blockData.text
+    .split("\n")
+    .filter(function (l) {
+      return !!l;
+    })
+    .forEach(function (l) {
+      const data = l.split("\t");
+      if (data.length !== 2) return;
 
-        const ipParts = data[0].split('.');
-        if (ipParts.length !== 4) return console.log(data[0], ipParts);
+      const ipParts = data[0].split(".");
+      if (ipParts.length !== 4) return console.log(data[0], ipParts);
 
-        if (!blocks[ipParts[0]]) blocks[ipParts[0]] = {};
-        if (!blocks[ipParts[0]][ipParts[1]]) blocks[ipParts[0]][ipParts[1]] = {};
+      if (!blocks[ipParts[0]]) blocks[ipParts[0]] = {};
+      if (!blocks[ipParts[0]][ipParts[1]]) blocks[ipParts[0]][ipParts[1]] = {};
 
-        blocks[ipParts[0]][ipParts[1]][ipParts[2] + '.' + ipParts[3]] = data[1];
+      blocks[ipParts[0]][ipParts[1]][ipParts[2] + "." + ipParts[3]] = data[1];
     });
 
-    const providerData = await superagent.get(APNIC_USED_AUTNUMS_URL);
-    providerData.text.split('\n').filter(function (l) { return !!l; }).forEach(function (l) {
-        const data = l.trim().split(' ');
-        if (data.length !== 3) return;
+  const providerData = await superagent.get(APNIC_USED_AUTNUMS_URL);
+  providerData.text
+    .split("\n")
+    .filter(function (l) {
+      return !!l;
+    })
+    .forEach(function (l) {
+      const data = l.trim().split(" ");
+      if (data.length !== 3) return;
 
-        // slice to remove trailing ,
-        provider[data[0]] = data[1].slice(0, -1);
+      // slice to remove trailing ,
+      provider[data[0]] = data[1].slice(0, -1);
     });
 
-    gCache = {
-        timestamp: Date.now(),
-        blocks,
-        provider
-    };
+  gCache = {
+    timestamp: Date.now(),
+    blocks,
+    provider,
+  };
 
-    fs.writeFileSync(gCacheFilePath, JSON.stringify(gCache), 'utf8');
+  fs.writeFileSync(gCacheFilePath, JSON.stringify(gCache), "utf8");
+}
+
+let updating = false;
+async function backgroundUpdate() {
+  if (updating) return;
+
+  const days = lastUpdated();
+  if (days >= 7) {
+    updating = true;
+    console.info("ASN cache out of date, attempting to update...");
+    await update().finally(() => (updating = false));
+  }
 }
 
 function lookup(ip) {
-    if (typeof ip !== 'string' || !ip) return 'unkown';
+  if (typeof ip !== "string" || !ip) return null;
 
-    const ipParts = ip.split('.');
+  const ipParts = ip.split(".");
 
-    const keyBlockFirst = Object.keys(gCache.blocks).find(function (p) { return p === ipParts[0]; });
-    if (!keyBlockFirst) return null;
-    const blockFirst = gCache.blocks[keyBlockFirst];
+  const keyBlockFirst = Object.keys(gCache.blocks).find(function (p) {
+    return p === ipParts[0];
+  });
+  if (!keyBlockFirst) return null;
+  const blockFirst = gCache.blocks[keyBlockFirst];
 
-    const keyBlockSecond = Object.keys(blockFirst).find(function (p) { return p === ipParts[1]; });
-    if (!keyBlockSecond) return null;
-    const blockSecond = blockFirst[keyBlockSecond];
+  const keyBlockSecond = Object.keys(blockFirst).find(function (p) {
+    return p === ipParts[1];
+  });
+  if (!keyBlockSecond) return null;
+  const blockSecond = blockFirst[keyBlockSecond];
 
-    const keyBlockRest = Object.keys(blockSecond).find(function (p) {
-        return ipcheck.match(ip, ipParts[0] + '.' + ipParts[1] + '.' + p);
-    });
+  const keyBlockRest = Object.keys(blockSecond).find(function (p) {
+    return ipcheck.match(ip, ipParts[0] + "." + ipParts[1] + "." + p);
+  });
 
-    if (!keyBlockRest) return null;
-    if (!gCache.provider[blockSecond[keyBlockRest]]) return 'unknown';
+  if (!keyBlockRest) return null;
+  return blockSecond[keyBlockRest];
+}
 
-    return gCache.provider[blockSecond[keyBlockRest]];
+function asnToName(asn) {
+  return gCache.provider[asn];
 }
 
 async function load() {
-    debug(`Using cache file path ${gCacheFilePath}`);
+  debug(`Using cache file path ${gCacheFilePath}`);
 
-    try {
-        gCache = JSON.parse(fs.readFileSync(gCacheFilePath, 'utf8'));
-    } catch (e) {
-        debug('No asnlookup cache found. Call update() first');
-    }
+  try {
+    gCache = JSON.parse(fs.readFileSync(gCacheFilePath, "utf8"));
+  } catch (e) {
+    debug("No asnlookup cache found. Call update() first");
+  }
 }
 
 exports = module.exports = function (cacheFilePath) {
-    gCacheFilePath = path.resolve(typeof cacheFilePath === 'string' && cacheFilePath ? cacheFilePath : '.asnlookup.cache');
+  gCacheFilePath = path.resolve(
+    typeof cacheFilePath === "string" && cacheFilePath
+      ? cacheFilePath
+      : ".asnlookup.cache"
+  );
 
-    return {
-        load,
-        lastUpdated,
-        update,
-        lookup
-    };
+  return {
+    load,
+    lastUpdated,
+    update,
+    lookup,
+    asnToName,
+    backgroundUpdate,
+  };
 };
--- test.js
+++ test.js
@@ -1,70 +1,72 @@
-'use strict';
+"use strict";
 
 /* global it:false */
 /* global describe:false */
 /* global before:false */
 /* global after:false */
 
-const expect = require('expect.js'),
-    fs = require('fs'),
-    path = require('path');
+const expect = require("expect.js"),
+  fs = require("fs"),
+  path = require("path");
 
-const TEST_CACHE_FILE = '.asnlookup.tests.cache';
+const TEST_CACHE_FILE = ".asnlookup.tests.cache";
 
 function cleanup() {
-    try {
-        fs.unlinkSync(path.resolve(TEST_CACHE_FILE));
-    } catch (e) {}
+  try {
+    fs.unlinkSync(path.resolve(TEST_CACHE_FILE));
+  } catch (e) {}
 }
 
-describe('asnlookup', function () {
-    this.timeout(10000);
+describe("asnlookup", function () {
+  this.timeout(10000);
 
-    before(cleanup);
-    after(cleanup);
+  before(cleanup);
+  after(cleanup);
 
-    it('last updated is Infinity without cache', function () {
-        const asnlookup = require('./index.js')(TEST_CACHE_FILE);
+  it("last updated is Infinity without cache", function () {
+    const asnlookup = require("./index.js")(TEST_CACHE_FILE);
 
-        const lastUpdated = asnlookup.lastUpdated();
+    const lastUpdated = asnlookup.lastUpdated();
 
-        expect(lastUpdated).to.equal(Infinity);
-    });
+    expect(lastUpdated).to.equal(Infinity);
+  });
 
-    it('can update cache initially', async function () {
-        this.timeout(0);
+  it("can update cache initially", async function () {
+    this.timeout(0);
 
-        const asnlookup = require('./index.js')(TEST_CACHE_FILE);
+    const asnlookup = require("./index.js")(TEST_CACHE_FILE);
 
-        console.log('Updating cache, this takes some time...');
-        await asnlookup.update();
+    console.log("Updating cache, this takes some time...");
+    await asnlookup.update();
 
-        const lastUpdated = asnlookup.lastUpdated();
+    const lastUpdated = asnlookup.lastUpdated();
 
-        expect(lastUpdated).to.equal(0);
-    });
+    expect(lastUpdated).to.equal(0);
+  });
 
-    it('can load from cache', async function () {
-        const asnlookup = require('./index.js')(TEST_CACHE_FILE);
+  it("can load from cache", async function () {
+    const asnlookup = require("./index.js")(TEST_CACHE_FILE);
 
-        await asnlookup.load();
+    await asnlookup.load();
 
-        const lastUpdated = asnlookup.lastUpdated();
+    const lastUpdated = asnlookup.lastUpdated();
 
-        expect(lastUpdated).to.equal(0);
-    });
+    expect(lastUpdated).to.equal(0);
+  });
 
-    it('lookup succeeds for 8.8.8.8', async function () {
-        const asnlookup = require('./index.js')(TEST_CACHE_FILE);
-        await asnlookup.load();
+  it("lookup succeeds for 8.8.8.8", async function () {
+    const asnlookup = require("./index.js")(TEST_CACHE_FILE);
+    await asnlookup.load();
 
-        expect(asnlookup.lookup('8.8.8.8')).to.equal('GOOGLE');
-    });
+    const asn = asnlookup.lookup("8.8.8.8");
+    expect(asn).to.equal("15169");
+    expect(asnlookup.asnToName(asn)).to.equal("GOOGLE");
+  });
 
-    it('lookup succeeds with null for 127.0.0.1', async function () {
-        const asnlookup = require('./index.js')(TEST_CACHE_FILE);
-        await asnlookup.load();
+  it("lookup succeeds with null for 127.0.0.1", async function () {
+    const asnlookup = require("./index.js")(TEST_CACHE_FILE);
+    await asnlookup.load();
 
-        expect(asnlookup.lookup('127.0.0.1')).to.eql(null);
-    });
+    expect(asnlookup.lookup("127.0.0.1")).to.eql(null);
+  });
 });
